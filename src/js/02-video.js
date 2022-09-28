f03e1500a1c98ac82d0536cc22a591317203ffcc
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2022-09-28 17:29:33 +0600

    Добавляет решение ко 2му заданию

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    
+    player.on('timeupdate', throttle((data) =>{
+    const time = data.seconds;
+    localStorage.setItem("videoplayer-current-time", time)
+    }, 1000));
+    const onTime = localStorage.getItem("videoplayer-current-time");
+
+    player.setCurrentTime(onTime).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'TypeError':
+                // the id was not a number
+                break;
+    
+            case 'PasswordError':
+                // the video is password-protected and the viewer needs to enter the
+                // password first
+                break;
+    
+            case 'PrivacyError':
+                // the video is password-protected or private
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+
 
